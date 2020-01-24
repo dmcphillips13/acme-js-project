a3e1315596ea9521fa2f52ad418890e83ad7c4c3
Denis McPhillips <denismcphillips@gmail.com>
2020-01-23 21:35:10 -0500

    final

--- app.js
+++ app.js
@@ -7,23 +7,39 @@ const employees = [
     { id: 6, name: 'harpo'},
     { id: 8, name: 'shep Jr.'},
     { id: 99, name: 'lucy'}
-];
+  ];
 
-const list = document.querySelector('ul');
-// const item = document.querySelectorAll('li')
+  const list = document.querySelector('#employees');
+  
+  list.addEventListener('click', (ev)=> {
+    const target = ev.target;
+    if(target.tagName === 'LI'){
+      const idx = [...list.children].indexOf(target);
+      employees[idx].favorite = !employees[idx].favorite;
+    }
+    render();
+  });
+  
+  const render = ()=> {
+    let counter = employees.reduce((accum, employee) => {
+        return employee.favorite ? accum + 1: accum;
+    }, 0)
+    
+    if (counter > 3) {
+        alert('No more than 3')
 
-const render = () => {
-    const html = employees.map(employee => {
-         return `<li>${employee.name}</li>`;  
+    } else {
+    const html = employees.map( employee => {
+        return `
+        <li data-id='${employee.id}' ${ employee.favorite ? "class='favorite'": ''}>${ employee.name }</li>
+        `;
     }).join('');
-
+    
     list.innerHTML = html;
-}
+    }
+  };
+  
+  render();
 
-render();
-
-list.addEventListener('click', (ev) => {
-    const.target = ev.target;
-    if(target.tagName === 'LI');
-    console.log(target)
-})
+  
+  
